64eb9cda8662d0b9f5663702d3e739e2028553bd
Selimhan Firat <selimhanfirat1@gmail.com>
2026-01-31 03:39:05 +0100

    improved testing

--- src/test/java/game/ClassicRules2048Test.java
+++ src/test/java/game/ClassicRules2048Test.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RulesTest {
+public class StandardRulesTest {
 
     private final Rules rules = new ClassicRules2048();
 
